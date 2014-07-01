dbbfd82b50f802838ad66389ebd098e103a9fd0d
Doug Hiebert <doug.hiebert@gmail.com>
2014-07-01 18:37:18 -0500

    Header links back to root page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def cms_index_url
+  	@cms_site.pages.root.url
+  end
+  
   def cms_url(page_label)
   	page = @cms_site.pages.find_by_label(page_label)
   	page ? page.url : 'page/missing'
